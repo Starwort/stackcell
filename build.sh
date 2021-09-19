7fa7edcb76cc8464e322f679494d29dc7c16408d
starwort <tcphone93@gmail.com>
2021-09-19 20:25:39 +0100

    Make the linux build executable

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 cargo build --release --target x86_64-unknown-linux-gnu
 cargo build --release --target x86_64-pc-windows-gnu
 cp target/x86_64-unknown-linux-gnu/release/stackcell dist/x86_64-unknown-linux-gnu/stackcell
+chmod +x dist/x86_64-unknown-linux-gnu/stackcell
 cp target/x86_64-pc-windows-gnu/release/stackcell.exe dist/x86_64-pc-windows-gnu/stackcell.exe
