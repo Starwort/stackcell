70654d39e6c8d5873024a192ec2cb57ac56b3191
starwort <tcphone93@gmail.com>
2021-09-22 01:33:28 +0100

    Build new files

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cargo build --release --target x86_64-unknown-linux-gnu
 cargo build --release --target x86_64-pc-windows-gnu
 cp target/x86_64-unknown-linux-gnu/release/stackcell dist/x86_64-unknown-linux-gnu/stackcell
